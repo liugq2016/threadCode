b276cd344608d4b68a1fbd345d83c386d0aab2ea
lgq <1241500152@qq.com>
2021-03-23 23:31:05 +0800

    add:P-20210323_信号量学习

---
+++ thirdpart/src/main/java/cn/lgq/third/SemapDemo.java
@@ -0,0 +1,36 @@
+package cn.lgq.third;
+
+import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+/**
+ * @author lgq
+ * @Description 允许多个线程同时访问:信号量(Semaphore)
+ * @create 2021-03-23 22:51
+ */
+public class SemapDemo implements Runnable{
+    final Semaphore semp = new Semaphore(5);
+    @Override
+    public void run() {
+        try {
+            semp.acquire();
+            Thread.sleep(2000);
+            System.out.println(Thread.currentThread().getId()+":done!");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            semp.release();
+        }
+    }
+
+    public static void main(String[] args) {
+        Date
+        ExecutorService exec = Executors.newFixedThreadPool(20);
+        final SemapDemo demp = new SemapDemo();
+        for (int i = 0;i<20;++i){
+            exec.submit(demp);
+        }
+    }
+}
